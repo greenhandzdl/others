a4edca85a96153f6d6bde0f6c21b6d1561c42474
greenhandzdl <greenhandzdl@gmail.com>
2022-01-15 12:24:43 +0800

    Remark

--- SHELL/Matlab_Shell/三角函数与单位圆关系/v_20220115.m
+++ SHELL/Matlab_Shell/三角函数与单位圆关系/v_20220115.m
@@ -2,15 +2,17 @@
 % Data:2022-1-15
 % Author:Greenhandzdl
 
-aplha = 0:pi/40:2*pi; %设置角度为(0,pi)精度pi/40
+aplha = 0:pi/40:2*pi; %设置角度为(0,2pi)精度pi/40
 r = 1; %单位圆半径为1
 cir_x = r*cos(aplha); %定义圆横坐标
 cir_y = r*sin(aplha); %定义圆纵坐标
 
-x = 0:pi/40:2*pi;
-c_x = cir_x*inv(r);
-s_y = cir_y*inv(r);
+x = 0:pi/40:2*pi; %一个普普通通的x轴
+c_x = cir_x*inv(r); %接受cos的函数
+s_y = cir_y*inv(r); %接受sin的函数
 
 plot(cir_x,cir_y, x,c_x, x,s_y ,'.-') ,legend('单位圆','余弦函数','正弦函数'), xlabel('横坐标代表cosx'),ylabel('纵坐标代表sinx'),title('正弦余弦的初识'),grid on, axis equal;
 
 clear all;
+
+%Feature(?):在单位圆上能出现一条可移动的横线来分别显示各个函数的值
