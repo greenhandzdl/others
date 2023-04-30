2c60b9eee1544ecb2ab6f8fcf5f5a8effa2686af
greenhandzdl <greenhandzdl@gmail.com>
2023-04-30 17:59:18 +0800

    Enhance

--- SHELL/Linux_Bash/Backup/arch_auto_init.sh
+++ SHELL/Linux_Bash/Backup/arch_auto_init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# Run the sh by default
+# Run the sh by default: default
+
+workdir=$(pwd)
 
 if [ $# -gt 0 ]; then
   choice=$1
@@ -70,7 +72,7 @@ if [[ -f /etc/arch-release ]]; then
     else
         read -p "zsh is not installed, do you want to install it? (y/n)" answer
         if [[ $answer == "y" ]] || [[ $choice == "default" ]]; then
-            yay -S zsh --noconfirm
+            su - $username -c yay -S zsh --noconfirm
         else
             echo "Skipping zsh installation"
         fi
@@ -93,7 +95,7 @@ if [[ -f /etc/arch-release ]]; then
     else
         read -p "Oh My Zsh is not installed, do you want to install it? (y/n)" answer
         if [[ $answer == "y" ]] || [[ $choice == "default" ]]; then
-            yay -S oh-my-zsh-git --noconfirm
+            su - $username -c yay -S oh-my-zsh-git --noconfirm
         else
             echo "it won't be installed."
         fi
@@ -108,7 +110,7 @@ if [[ -f /etc/arch-release ]]; then
         if [ "$answer" = "y" ] || [ "$answer" = "Y" ] || [[ $choice == "default" ]]
         then
             # Install lolcat using yay package manager
-            yay -S lolcat --noconfirm
+            su - $username -c yay -S lolcat --noconfirm
         else
             echo "Skipping Lolcat installation."
         fi
@@ -161,7 +163,7 @@ if [[ -f /etc/arch-release ]]; then
       read -p "Do you want to install Cockpit? [y/N] " confirm
       if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]|| [[ $choice == "default" ]]; then
         echo "Installing Cockpit..."
-        yay -S cockpit --noconfirm
+        su - $username -c yay -S cockpit --noconfirm
       else
         echo "Skipping Cockpit installation"
       fi
@@ -210,7 +212,7 @@ if [[ -f /etc/arch-release ]]; then
       read -p "Do you want to install GDM? [y/N] " confirm
       if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]|| [[ $choice == "default" ]]; then
         echo "Installing GDM..."
-        yay -S gdm --noconfirm
+        su - $username -c yay -S gdm --noconfirm
         # Check if the GDM service is enabled after installing GDM
         if systemctl is-enabled gdm > /dev/null ; then
           echo "GDM service is enabled"
@@ -232,10 +234,9 @@ if [[ -f /etc/arch-release ]]; then
     if [ "$XDG_CURRENT_DESKTOP" != "GNOME" ]; then
         # Prompt the user to install GNOME
         read -p "Do you want to install GNOME? [Y/n] " -n 1 -r
-        echo
         # Install GNOME if the user agrees
         if [[ $REPLY =~ ^[Yy]$ ]] || [[ -z $REPLY ]]|| [[ $choice == "default" ]]; then
-            yay -S gnome --noconfirm
+            su - $username -c yay -S gnome --noconfirm
         fi
     fi
 
@@ -247,12 +248,12 @@ if [[ -f /etc/arch-release ]]; then
         if [ "$answer" == "Y" ] || [ "$answer" == "y" ]|| [[ $choice == "default" ]]
         then
             echo "Installing software..."
-            yay -S --noconfirm fcitx5 	fcitx5-configtool fcitx5-gtk fcitx5-qt
-            yay -S --noconfirm fcitx5-chewing fcitx5-chinese-addons fcitx5-rime fcitx5-mcbopomofo-git  fcitx5-anthy  fcitx5-kkc fcitx5-mozc fcitx5-skk
-            yay -S --noconfirm fcitx5-input-support fcitx5-pinyin-zhwiki fcitx5-pinyin-sougou
-            yay -S --noconfirm fcitx5-pinyin-moegirl-rime cedict fcitx5-pinyin-moegirl libime
-            yay -S --noconfirm fcitx5-skin-zhuobihaotian-luotianyi
-            yay -S --noconfirm kimpanel
+            su - $username -c yay -S --noconfirm fcitx5 	fcitx5-configtool fcitx5-gtk fcitx5-qt
+            su - $username -c yay -S --noconfirm fcitx5-chewing fcitx5-chinese-addons fcitx5-rime fcitx5-mcbopomofo-git  fcitx5-anthy  fcitx5-kkc fcitx5-mozc fcitx5-skk
+            su - $username -c yay -S --noconfirm fcitx5-input-support fcitx5-pinyin-zhwiki fcitx5-pinyin-sougou
+            su - $username -c yay -S --noconfirm fcitx5-pinyin-moegirl-rime cedict fcitx5-pinyin-moegirl libime
+            su - $username -c yay -S --noconfirm fcitx5-skin-zhuobihaotian-luotianyi
+            su - $username -c yay -S --noconfirm kimpanel
             # Add commands to install software here
         elif [ "$answer" == "N" ] || [ "$answer" == "n" ]
         then
@@ -265,113 +266,19 @@ if [[ -f /etc/arch-release ]]; then
         if [ "$answer" == "Y" ] || [ "$answer" == "y" ]|| [[ $choice == "default" ]]
         then
             echo "Installing software..."
-            yay -S --noconfirm albert plank-git conky
+            su - $username -c yay -S --noconfirm albert plank-git conky
             #tools
-            yay -S --noconfirm ghostlexly-gpu-video-wallpaper gdm-settings gnome-tweaks-git gnome-session-properties
+            su - $username -c yay -S --noconfirm ghostlexly-gpu-video-wallpaper gdm-settings gnome-tweaks-git gnome-session-properties
             # theme pack
-            yay -S --noconfirm xcursor-genshin-nahida apricity-icons mojave-gtk-theme-git
+            su - $username -c yay -S --noconfirm xcursor-genshin-nahida apricity-icons mojave-gtk-theme-git
             #extension
-            yay -S --noconfirm gnome-shell-extension-clipboard-indicator compiz gnome-shell-extension-dynamic-panel-transparency-git gnome-shell-extension-simple-system-monitor
+            su - $username -c yay -S --noconfirm gnome-shell-extension-clipboard-indicator compiz gnome-shell-extension-dynamic-panel-transparency-git gnome-shell-extension-simple-system-monitor
             #fonts
-            yay -S --noconfirm 	ttf-mac-fonts
+            su - $username -c yay -S --noconfirm 	ttf-mac-fonts
             if [ -f ~/.conkyrc ]; then
                 mv ~/.conkyrc ~/.conkyrc.backup
             fi
-              echo "conky.config = {
-                alignment = 'top_right',
-                background = false,
-                border_width = 1,
-                cpu_avg_samples = 2,
-                default_color = 'white',
-                default_outline_color = 'white',
-                default_shade_color = 'white',
-                double_buffer = true,
-                draw_borders = false,
-                draw_graph_borders = true,
-                draw_outline = false,
-                draw_shades = false,
-                extra_newline = false,
-                font = 'Noto Sans Mono:size=12',
-                gap_x = 20,
-                gap_y = 20,
-                minimum_height = 5,
-                minimum_width = 5,
-                net_avg_samples = 2,
-                no_buffers = true,
-                out_to_console = false,
-                out_to_ncurses = false,
-                out_to_stderr = false,
-                out_to_x = true,
-                own_window = true,
-                own_window_argb_value = 0,
-                own_window_argb_visual = true,
-                own_window_class = 'Conky',
-                own_window_hints = 'undecorated,sticky,below,skip_taskbar,skip_pager',
-                own_window_transparent = true,
-                own_window_type = 'desktop',
-                show_graph_range = false,
-                show_graph_scale = false,
-                stippled_borders = 0,
-                update_interval = 2.0,
-                uppercase = false,
-                use_spacer = 'none',
-                use_xft = true,
-            }
-            conky.text = [[${voffset 4}${color}${font :bold}HP EliteBook 840 G3${font}${image /home/username/.config/conky/archlogo.png -n -p 280x0 -s 98x30}
-            $hr
-            ${color grey}Hostname: ${color}$nodename
-            ${color grey}OS      : ${color}$sysname $kernel $machine
-            ${color grey}Uptime  : ${color}$uptime
-            ${color grey}Battery : ${color}$battery ${voffset 3}${battery_bar}${voffset -3}
-            ${voffset 8}${color}${font :size=14}CPU ${font} (Intel Core i7-6500U)$hr
-            ${voffset 4}${color grey}Core 0: ${color}${alignr}${freq_g 1} GHz  ${color grey}Temperature: ${color}${execi 30 sensors | grep "Core 0" | awk '{print $3}'}
-            ${color grey}Core 1: ${color}${alignr}${freq_g 2} GHz  ${color grey}Temperature: ${color}${execi 30 sensors | grep "Core 1" | awk '{print $3}'}
-            ${voffset 4}${color grey}Thread 1: ${color}${voffset 3}${cpubar cpu1}${voffset -3}
-            ${color grey}Thread 2: ${color}${voffset 3}${cpubar cpu2}${voffset -3}
-            ${color grey}Thread 3: ${color}${voffset 3}${cpubar cpu3}${voffset -3}
-            ${color grey}Thread 4: ${color}${voffset 3}${cpubar cpu4}${voffset -3}
-            ${voffset 4}${color grey}Process              CPU%   MEM%
-            ${color}  ${top name 1} ${top cpu 1} ${top mem 1}
-            ${color}  ${top name 2} ${top cpu 2} ${top mem 2}
-            ${color}  ${top name 3} ${top cpu 3} ${top mem 3}
-            ${voffset 6}${color}${font :size=14}RAM ${font}$hr
-            ${voffset 4}${color grey}Memory usage: ${color}${memperc}% of ${memmax} ${voffset 3}${membar}${voffset -3}
-            ${voffset 4}${color grey}Process              MEM%   CPU%
-            ${color}  ${top_mem name 1} ${top_mem mem 1} ${top_mem cpu 1}
-            ${color}  ${top_mem name 2} ${top_mem mem 2} ${top_mem cpu 2}
-            ${color}  ${top_mem name 3} ${top_mem mem 3} ${top_mem cpu 3}
-            ${voffset 6}${color}${font :size=14}DISK ${font}$hr
-            ${voffset 4}${color grey}Local root: $color${fs_free /} ${goto 215}${color grey}/ ${color} ${fs_size /}${goto 335}${voffset 3}${fs_bar /}${voffset -3}
-            ${color grey}Gentoo    : ${color}${fs_free /mnt/Gentoo} ${goto 215}${color grey}/ ${color}${fs_size /mnt/Gentoo}${goto 335}${voffset 3}${fs_bar /mnt/Gentoo}${voffset -3}
-            ${color grey}Google    : ${color}${fs_free /home/username/Cloud/GoogleDrive} ${goto 215}${color grey}/ ${color}${fs_size /home/username/Cloud/GoogleDrive}${goto 335}${voffset 3}${fs_bar /home/username/Cloud/GoogleDrive}${voffset -3}
-            ${color grey}OneDrive  : $color${fs_free /home/username/Cloud/OneDrive-Private} ${goto 215}${color grey}/ ${color}${fs_size /home/username/Cloud/OneDrive-Private}${goto 335}${voffset 3}${fs_bar /home/username/Cloud/OneDrive-Private}${voffset -3}
-            ${color grey}Dropbox   : $color${fs_free /home/username/Cloud/Dropbox} ${goto 215}${color grey}/ ${color}${fs_size /home/username/Cloud/Dropbox}${goto 335}${voffset 3}${fs_bar /home/username/Cloud/Dropbox}${voffset -3}
-            ${color grey}Mega      : $color${fs_free /home/username/Cloud/Mega} ${goto 215}${color grey}/ ${color}${fs_size /home/username/Cloud/Mega}${goto 335}${voffset 3}${fs_bar /home/username/Cloud/Mega}${voffset -3}
-            ${color grey}Pelle     : $color${fs_free /mnt/Pelle/Felles} ${goto 215}${color grey}/ ${color}${fs_size /mnt/Pelle/Felles}${goto 335}${voffset 3}${fs_bar /mnt/Pelle/Felles}${voffset -3}
-            ${voffset 4}${color grey}      Read: ${color}${diskio_read /dev/sda} ${goto 230}${color grey}Write: ${color}${diskio_read /dev/sda}
-            ${voffset 6}${color}${font :size=14}NETWORK ${font}$hr
-            ${if_existing /proc/net/route wlp2s0}\
-            ${if_existing /proc/net/route enp0s31f6}\
-            ${voffset 4}${color grey}Warning: ${color}Both Wi-Fi and wired network active!
-            ${else}\
-            ${voffset 4}${color grey}ESSID: ${color}${wireless_essid wlp2s0}${goto 240}${color grey}Quality: ${color}${wireless_link_qual_perc wlp2s0}%
-            ${voffset 4}${color grey}  IP : ${color}${addr wlp2s0}${goto 240}${voffset 8}${color grey}Down:${goto 300}${color}${downspeed wlp2s0}
-            ${voffset -8}${color grey}  Zt : ${color}${addr zerotierdevice}${goto 240}${voffset 8}${color grey}Up  :${goto 300}${color}${upspeed wlp2s0}
-            ${voffset -8}${color grey}  GW : ${color}${gw_ip}
-            ${color grey}  Pub: ${color}${texeci 30 ~/.config/conky/pubIP.sh}
-            ${endif}\
-            ${else}\
-            ${if_existing /proc/net/route enp0s31f6}\
-            ${voffset 4}${color grey}Wired network: ${color}Active
-            ${voffset 4}${color grey}  IP : ${color}${addr enp0s31f6}${goto 240}${voffset 8}${color grey}Down:${goto 300}${color}${downspeed enp0s31f6}
-            ${voffset -8}${color grey}  Zt : ${color}${addr zerotierdevice}${goto 240}${voffset 8}${color grey}Up  :${goto 300}${color}${upspeed enp0s31f6}
-            ${voffset -8}${color grey}  GW : ${color}${gw_ip}
-            ${color grey}  Pub: ${color}${texeci 30 ~/.config/conky/pubIP.sh}
-            ${endif}\
-            ${endif}\
-            ${voffset 6}${color}${font :size=14}UPDATES ${font}$hr
-            ${voffset 4}${color grey}      Arch: ${color}${execi 600 checkupdates | wc -l}${goto 230}${color grey}AUR: ${color}${execi 600 auracle outdated | wc -l}
-            ]]" > ~/.conkyrc
+                cp $workdir/config/.conkyrc  ~/.conkyrc
         elif [ "$answer" == "N" ] || [ "$answer" == "n" ]
         then
             echo "Skipping installation..."
@@ -382,7 +289,7 @@ if [[ -f /etc/arch-release ]]; then
         if [ "$answer" == "Y" ] || [ "$answer" == "y" ]|| [[ $choice == "default" ]]
         then
             echo "Installing software..."
-            yay -S --noconfirm gnome-shell-extension-gsconnect google-chrome libreoffice-fresh vim neovim visual-studio-code-bin
+            su - $username -c yay -S --noconfirm gnome-shell-extension-gsconnect google-chrome libreoffice-fresh vim neovim visual-studio-code-bin
         elif [ "$answer" == "N" ] || [ "$answer" == "n" ]
         then
             echo "Skipping installation..."
