a39f4f41abb824643c8b8485e9f04f15f2bc914a
greenhandzdl <greenhandzdl@gmail.com>
2024-09-20 16:47:03 +0800

    All done

--- c/stdio.c
+++ c/stdio.c
@@ -14,13 +14,21 @@ int main(void){
 
 int stdio(void){
     int ch = '0';
+    /*
+    printf("请输入任意字符并且按下回车键，程序会进行复读哦～\n");
+    printf("退出某项复读的方式是模拟EOF信号\n");
+    printf("正在复读:\n");
+    while ((ch=getchar()) != EOF)
+        putchar(ch);
+    */
     do{
         printf("请输入任意字符并且按下回车键，程序会进行复读哦～\n");
         printf("退出某项复读的方式是模拟EOF信号\n");
         printf("正在复读:\n");
         while ((ch=getchar()) != EOF)
             putchar(ch);
-        while(!isspace(ch=getchar()));
+        clearerr(stdin);//清理被程序设置stdin关闭的标志，打开标准输入
+        //while(!isspace(ch=getchar()));//似乎没什么关系，因为ctrl d并不会加入什么blank和space
         printf("\n输入q进行退出：\n");
         switch (toupper(ch=getchar())){
             //if处理更好，也可以尝试switch语句不用break中断来连接几个选项
@@ -31,6 +39,5 @@ int stdio(void){
                 break;
         }
     }while(1);
-
     quit:return 0;
 }
