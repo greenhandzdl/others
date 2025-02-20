de859f0d7a6bfd95b6208136d8b08b59a6a76ca7
greenhandzdl <greenhandzdl@gmail.com>
2025-02-20 22:34:03 +0800

    something news

--- CPP/courseRequirements/CircleClass/main.cpp
+++ CPP/courseRequirements/CircleClass/main.cpp
@@ -1,43 +1,67 @@
 #include<iostream>
 #include<iomanip>
+namespace CircleNamespace{
+    typedef long double dat;
+    dat operator""_m(dat value){
+        return static_cast<dat>(value * 1);
+    }
+    dat operator""_dm(dat value){
+        return static_cast<dat>(value * 1E-1);
+    }
 
-class Circle{
-    private:
-        typedef double dat;
-        const dat PI = 3.14159265358979323846;
-        dat radius;
-        dat area;
-        dat circumference;
-    public:
-        Circle(dat&& r = 0) : radius(r),area(r*r*PI),circumference(2*r*PI){
-            std::cout << "Circle constructor" << std::endl;
-        }
-        Circle(Circle& c) = default;
-        Circle(Circle&& c) = default;
-        Circle& operator=(const Circle& other) = default;
-        Circle& operator=(Circle&& other) = default;
-        virtual ~Circle(){
-            std::cout << "Circle destructor" << std::endl;
-        }
-        void show(){
-            using namespace std;
-            cout << setprecision(3) << fixed
-            << setw(16) << "radius: " << setw(8) <<radius << endl
-            << setw(16) << "area: " << setw(8) <<area << endl
-            << setw(16) << "circumference: " << setw(8) <<circumference << endl;
-        }
-        friend std::ostream& operator<<(std::ostream& os, Circle& c){
-            using namespace std;
-            os << setprecision(3) << fixed
-            << setw(16) << "radius: " << setw(8) <<c.radius << endl
-            << setw(16) << "area: " << setw(8) <<c.area << endl
-            << setw(16) << "circumference: " << setw(8) <<c.circumference << endl;
-            return os;
-        }
-};
-
+    dat operator""_cm(dat value){
+        return static_cast<dat>(value * 1E-2);
+    }
+    dat operator""_mm(dat value){
+        return static_cast<dat>(value * 1E-3);
+    }
+    
+    class Circle{
+        private:
+            constexpr static dat PI = 3.14159265358979323846;
+            static unsigned int count;
+            dat radius;
+            dat area;
+            dat circumference;
+        public:
+            Circle(dat&& r = 0) : radius(r),area(r*r*PI),circumference(2*r*PI){
+                count++;
+                std::cout << "#" << count << "Circle constructor" << std::endl;
+            }
+            operator dat(){
+                return this->radius;
+            }
+            Circle(Circle& c) = delete;
+            Circle(Circle&& c) = default;
+            Circle& operator=(const Circle& other) = delete;
+            Circle& operator=(Circle&& other) = default;
+            virtual ~Circle(){
+                count--;
+                std::cout << "#" << count << "Circle destructor" << std::endl;
+            }
+            void show(){
+                using namespace std;
+                cout << setprecision(3) << fixed
+                << setw(16) << "#" << count << "Circle" << endl
+                << setw(16) << "radius: " << setw(8) <<radius << endl
+                << setw(16) << "area: " << setw(8) <<area << endl
+                << setw(16) << "circumference: " << setw(8) <<circumference << endl;
+            }
+            friend std::ostream& operator<<(std::ostream& os, Circle& c){
+                using namespace std;
+                os << setprecision(3) << fixed
+                << setw(16) << "#" << count << "Circle" << endl
+                << setw(16) << "radius: " << setw(8) <<c.radius << endl
+                << setw(16) << "area: " << setw(8) <<c.area << endl
+                << setw(16) << "circumference: " << setw(8) <<c.circumference << endl;
+                return os;
+            }   
+    };
+    unsigned int Circle::count = 0;
+}
 int main(void){
-    Circle c1 = 5,c2 = c1;
+    using namespace CircleNamespace;
+    Circle c1 = 5.0_cm,c2 = c1 + 5.0_m;
     c1.show();
     c2.show();
 }
