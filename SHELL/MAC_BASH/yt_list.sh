3348dfc713310218b4c97b0fe47951ae02451166
greenhandzdl <greenhandzdl@gmail.com>
2025-05-16 21:56:13 +0800

    with srt support

--- SHELL/MAC_BASH/yt_list.sh
+++ SHELL/MAC_BASH/yt_list.sh
@@ -12,8 +12,18 @@ playlist_url="$1"
 
 # 使用yt-dlp下载播放列表
 yt-dlp -f 'bv*[height<=1080]+ba/b[height<=1080] / bv*+ba/b' --download-archive videos.txt "$playlist_url" -o '%(channel_id)s/%(playlist_id)s/%(title)s_%(id)s.%(ext)s' --cookies-from-browser chrome 
+yt-dlp --skip-download --write-subs --sub-langs en,zh.* --convert-subs srt -o '%(channel_id)s/%(playlist_id)s/%(title)s_%(id)s.%(ext)s' "$playlist_url" --cookies-from-browser chrome
 
 
+# 带字幕
+# yt-dlp -f 'bv*[height<=1080]+ba/b[height<=1080] / bv*+ba/b' --download-archive videos.txt "$playlist_url" -o '%(channel_id)s/%(playlist_id)s/%(title)s_%(id)s.%(ext)s' --cookies-from-browser chrome --write-subs --sub-langs en,zh.* --convert-subs srt
+
+
+# 可能更好的选择(但不是list)
+# yt-dlp -k --write-subs --sub-langs &#34;en&#34; --convert-subs srt $playlist_url
+
+# Skip版本
+# yt-dlp --skip-download --write-auto-subs --sub-langs &#34;zh-Hans-en&#34; --convert-sub srt
 
 
 
